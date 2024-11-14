76fad1828f02655cedcbbaaf2467562afec785e8
Jonah Kim <j2.web.apps@gmail.com>
2024-11-13 19:24:31 -0600

    init

---
+++ utils/bookingLogic.js
@@ -0,0 +1,95 @@
+import axios from "axios";
+import FormData from "form-data";
+import { slotParser } from "./slotParser.js";
+import { convertDateToLongFormat } from "./helpers.js";
+import {
+  existingReservationConfig,
+  slotConfig,
+  bookingConfig,
+  finalConfig,
+} from "../config.js";
+
+// First, we'll see if we already have a reservation
+async function checkForExistingBooking() {
+  let config = existingReservationConfig(process.env.AUTH_TOKEN);
+  let venueId = process.env.VENUE_ID;
+  try {
+    const response = await axios.request(config);
+    if (response.data.reservations[0]?.venue?.id == venueId) {
+      console.log(`You already have a reservation for tonight!`);
+      return true;
+    } else {
+      console.log(response.data.reservations[0]?.venue?.id)
+      console.log('Found no reservations')
+      return false;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Then, we'll check to see if there are any reservations available
+async function fetchDataAndParseSlots() {
+  try {
+    const response = await axios.request(slotConfig);
+    if (response.data.results.venues.length === 0) {
+      console.log(
+        "No slots available. Please run again after reservations open.",
+      );
+      return false;
+    }
+    console.log(
+      `Checking for reservations at ${
+        response.data.results.venues[0].venue.name
+      } on ${convertDateToLongFormat(process.env.DATE)} for ${
+        process.env.PARTY_SIZE
+      } people...`,
+    );
+    let slots = response.data.results.venues[0].slots;
+    const slotId = await slotParser(slots);
+    return slotId;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// If there are reservations available, we'll grab the booking token
+async function getBookingConfig(slotId) {
+  try {
+    const response = await axios.request(bookingConfig(slotId));
+    return response.data.book_token.value;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Finally, we'll make the reservation
+async function makeBooking(book_token) {
+  let config = finalConfig(process.env.AUTH_TOKEN);
+  const formData = new FormData();
+  formData.append(
+    "struct_payment_method",
+    JSON.stringify({ id: process.env.PAYMENT_ID }),
+  );
+  formData.append("book_token", book_token);
+  formData.append("source_id", "resy.com-venue-details");
+
+  try {
+    const response = await axios.post(config.url, formData, {
+      headers: {
+        ...config.headers,
+        ...formData.getHeaders(),
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error.response.data);
+  }
+}
+
+export {
+  checkForExistingBooking,
+  fetchDataAndParseSlots,
+  getBookingConfig,
+  makeBooking,
+};
---
+++ utils/helpers.js
@@ -0,0 +1,65 @@
+import jwt from 'jsonwebtoken';
+
+function convertTimeToTwelveHourFormat(time) {
+  const timeString = time.split(' ')[1];
+  const [hour, minutes] = timeString.split(':');
+  const suffix = hour >= 12 ? 'PM' : 'AM';
+  const hour12 = hour % 12 || 12;
+  return `${hour12}:${minutes} ${suffix}`;
+}
+
+function convertDateToLongFormat(date) {
+  const dateArray = date.split('-');
+  const [year, month, day] = dateArray;
+  const dateObject = new Date(year, month - 1, day);
+  const options = { weekday: 'long', month: 'long', day: 'numeric' };
+  return dateObject.toLocaleDateString('en-US', options);
+}
+
+function isTimeBetween(startTime, endTime, dateString) {
+  let targetTime = dateString.split(' ')[1];
+
+  // Convert times to minutes since midnight
+  const convertToMinutes = (timeStr) => {
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+  };
+
+  const startMinutes = convertToMinutes(startTime);
+  const endMinutes = convertToMinutes(endTime);
+  const targetMinutes = convertToMinutes(targetTime);
+  return targetMinutes >= startMinutes && targetMinutes <= endMinutes;
+}
+
+async function checkTokenExpiration(token) {
+  if (!token) {
+    console.error('JWT token not found in the AUTH_TOKEN environment variable');
+    process.exit(1);
+  }
+
+  try {
+    const decoded = jwt.decode(token);
+
+    if (decoded) {
+      const currentTimestamp = Math.floor(Date.now() / 1000);
+      const timeUntilExpiration = decoded.exp - currentTimestamp;
+      const expirationDate = new Date(decoded.exp * 1000);
+
+      if (timeUntilExpiration <= 0) {
+        console.log('JWT has already expired');
+        return false;
+      } else {
+        console.log(`JWT will expire on ${expirationDate}`);
+        return true;
+      }
+    } else {
+      console.error('JWT decoding failed');
+      return false;
+    }
+  } catch (error) {
+    console.error('JWT decoding failed:', error);
+    return false;
+  }
+}
+
+export { convertTimeToTwelveHourFormat, convertDateToLongFormat, isTimeBetween, checkTokenExpiration };
